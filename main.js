fe45e6d9fd5751510c04a715479d42b3a497ee2d
Mitchell <jeffrey.mitchell@accenture.com>
2024-09-23 15:51:32 -0400

    first update

---
+++ main.js
@@ -0,0 +1,42 @@
+// Arrays
+// This will be followed by loops
+
+ // Unfortunately, strings and numbers are not enough for most programming purposes.
+ //What is needed are  collections of data that can be used efficiently.
+ //This is where arrays come in.
+
+ //- Great for storing DataTransfer
+ //= Enumberating data, using and index to find them.
+ // - Quickly reordering data
+ 
+ //[] - square brackets are used.
+ // brackets hold more than one piece of data.
+
+//let friends = ['Moe', 'Curly', 'Larry']
+//console.log(friends[0]);
+//let firstFriend = [0];
+//let lastFriend = [2];
+//console.log(lastFriend);
+// console.log(friends);
+
+//let  nums = [22, 48, 4, 80];
+// note mixing data types in an array is allowed only in Javascript.
+
+// Created an array using bracket notation
+let shoeBrands = []
+shoeBrands[0] = 'Brooks'
+console.log(shoeBrands)
+shoeBrands[1] = 'Hoka';
+shoeBrands[2] = 'NB';
+shoeBrands[10] = 'Adidas'
+console.log(shoeBrands);
+console.log(shoeBrands.length);
+
+// Create an array of clothing brands
+// Create an array of car brands using
+
+clothingBrands = ['Levi', 'Kirkland', 'Adidas']
+carBrands = ['Audi','BMW','Chevorlet', 'Dodge', 'Ford','Honda','Isuzu','Jaguar', 'Lincoln', 'Maybach']
+
+// JS Loops
+// Allows you to get tasks done repetitively and accurately.
